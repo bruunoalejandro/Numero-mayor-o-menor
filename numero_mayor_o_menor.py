a23fc6b168df3273c6e828db9768d4d20bb1ee11
Bruno Fernandes <125995286+bruunoalejandro@users.noreply.github.com>
2023-02-21 14:55:20 -0300

    Add files via upload

---
+++ numero_mayor_o_menor.py
@@ -0,0 +1,8 @@
+print("Ingrese un numero")
+x = float(input())
+print("Ingrese otro numero")
+y = float(input())
+if x > y:
+    print("El primer numero es mayor que el segundo")
+if y > x:
+    print("El segundo numero es mayor que el primer")
